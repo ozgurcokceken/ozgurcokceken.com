5d2c40470334f867eee0f6f0adfc9c8a60360ac4
dufren <ozgurcokckn@gmail.com>
2024-03-16 05:06:06 +0300

    ready to deploy

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="my-5 text-center">
+      <hr className="my-5" />
+      <h1>2024 © Özgür Çokçeken</h1>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,102 @@
-import Image from "next/image";
+import Link from "next/link";
+
+const experiences = [
+  {
+    time: "March 2024 - Current",
+    company: {
+      name: "Octet Turkey",
+      url: "https://www.octet.com",
+      title: "Specialist Frontend Developer",
+    },
+    description:
+      "I joined Octet Turkey as a Specialist Frontend Developer. Here, I focused on developing user-friendly and interactive interfaces using modern web technologies in the financial technology field.",
+  },
+  {
+    time: "May 2023 - March 2024 · 11 Month",
+    company: {
+      name: "ParamTech",
+      url: "https://param.eu",
+      title: "Junior Frontend Developer",
+    },
+    description:
+      "My first step into Frontend Developer carrer. I worked on developing project called 'BackOffice'. Project mainly focuses internal staff needs. I learned a lot about React.js and Next.js during this time. I also had the opportunity to work with a great team.",
+  },
+  {
+    time: "June 2022 - May 2023 · 1 Year",
+    company: {
+      name: "Istinye University",
+      url: "https://www.istinye.edu.tr/en",
+      title: "Web Developer",
+    },
+    description:
+      "Shortly before graduation, I started working as a Web Developer at my university. During this time, while maintaining the university&apos;s needs, I also began learning React and successfully completed courses provided by Meta, earning certificates.",
+  },
+  {
+    time: "March 2022 - June 2022 · 4 Month",
+    company: {
+      name: "Finartz",
+      url: "https://finartz.com",
+      title: "Intern",
+    },
+    description: "I joined Finartz for my university internship program.",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <article>
+        <div className="text-center sm:text-left">
+          <p className="mb-5 text-md sm:text-xl md:text-2xl lg:text-3xl 2xl:text-4xl">
+            Hey👋 I&apos;m a frontend developer based in Turkey, Istanbul.
+          </p>
+
+          <p className="mb-5 text-md sm:text-xl md:text-2xl lg:text-3xl 2xl:text-4xl">
+            I develop things as a Specialist Frontend Developer at{" "}
+            <span className="font-extrabold">Octet Turkey</span>. Previously, I
+            worked as a Junior Frontend Developer at{" "}
+            <span className="font-extrabold">ParamTech</span>.
+          </p>
+
+          <p className="mb-5 text-md sm:text-xl md:text-2xl lg:text-3xl 2xl:text-4xl">
+            I enjoy working with{" "}
+            <span className="font-extrabold">React.js</span> &{" "}
+            <span className="font-extrabold">Next.js</span> and crafting
+            beatiful frontend experiences.
+          </p>
         </div>
-      </div>
+      </article>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <hr />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="my-5">
+        <h1 className="mb-5 text-2xl md:text-2xl lg:text-3xl 2xl:text-4xl font-semibold text-center sm:text-left">
+          Experience
+        </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <ol className="relative border-s border-black">
+          {experiences.map((experience, index) => (
+            <li key={index} className="mb-10 ms-4">
+              <div className="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 bg-black"></div>
+              <time className="mb-1 text-sm md:text-md lg:text-xl font-normal leading-none text-gray-500">
+                {experience.time}
+              </time>
+              <h3 className="text-md md:text-lg lg:text-2xl font-semibold">
+                <Link
+                  className="relative w-fit after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
+                  href={experience.company.url}
+                  target="_blank"
+                >
+                  {experience.company.name}
+                </Link>{" "}
+                - {experience.company.title}
+              </h3>
+              <p className="mb-4 text-md md:text-lg lg:text-xl font-normal text-gray-700">
+                {experience.description}
+              </p>
+            </li>
+          ))}
+        </ol>
       </div>
     </main>
   );
